1602ab6c54c07cb066b7e7ab710d5966f51b1d84
fyzgaston <fyzgaston@gmail.com>
2025-07-23 12:43:11 +0500

    use HashRouter for GitHub Pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,17 @@
 import './styles';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import MusicApp from './MusicApp'
 import { QueryClientProvider } from '@tanstack/react-query';
 import {queryClient} from '@/shared/lib/queryClient.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='vibecast-studio'>
+    <HashRouter basename='vibecast-studio'>
       <QueryClientProvider client={queryClient}>
         <MusicApp />
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
